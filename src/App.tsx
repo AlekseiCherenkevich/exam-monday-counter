cfd372f93cbca22ff75679646d24e73d3a50c5da
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2022-11-04 20:27:52 +0300

    fix disabled

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Count from "./components/Count";
 import Button from "./components/Button";
 
 function App() {
-    const [count, setCount] =useState<number>(0)
+    const [count, setCount] = useState<number>(0)
     const increase = () => {
         if (count < 5) {
             setCount(count + 1)
@@ -14,15 +14,15 @@ function App() {
     const reset = () => {
         setCount(0)
     }
-  return (
-    <div className="App">
-        <Count fontColor={count === 5 ? 'red' : 'black'} count={count}/>
-      <>
-          <Button isDisabled={count === 5} callback={increase}>inc</Button>
-          <Button isDisabled={count === 0} callback={reset}>reset</Button>
-      </>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Count fontColor={count === 5 ? 'red' : 'black'} count={count}/>
+            <>
+                <Button isDisabled={count === 5} callback={increase}>inc</Button>
+                <Button isDisabled={count === 0} callback={reset}>reset</Button>
+            </>
+        </div>
+    );
 }
 
 export default App;
